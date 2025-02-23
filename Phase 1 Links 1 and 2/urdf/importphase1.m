4b0cd213a02818c2e95cffe48d4cf40316315f32
giacomo-cristiano <giacomo.cristiano@mail.utoronto.ca>
2025-02-22 22:10:27 -0500

    1st Change - MATLAB URDF IMPORTS
    Adding in first and second phase of robot arm URDF

---
+++ Phase 1 Links 1 and 2/urdf/importphase1.m
@@ -0,0 +1,6 @@
+%%
+% Load the robot
+robot = importrobot('C:\Users\User\OneDrive - University of Toronto\4th year TERM 1\APS 490 CAPSTONE\Robot Modellig\Phase 1 Links 1 and 2\urdf\Phase 1 Links 1 and 2.urdf');
+
+% Show robot model
+show(robot);
---
+++ Phase 2 ALL LINKS/urdf/importphase2.m
@@ -0,0 +1,6 @@
+%%
+% Load the robot
+robot = importrobot('C:\Users\User\OneDrive - University of Toronto\4th year TERM 1\APS 490 CAPSTONE\Robot Modellig\Phase 2 ALL LINKS\urdf\Phase 2 ALL LINKS.urdf');
+
+% Show robot model
+show(robot);
